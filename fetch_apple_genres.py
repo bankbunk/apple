5f230f8a8d4f01ba7195044efd644f480fdc1f23
bankbunk <khonamriinsta@gmail.com>
2025-12-14 13:55:35 +0000

    Update fetch_apple_genres.py

--- fetch_apple_genres.py
+++ fetch_apple_genres.py
@@ -16,7 +16,7 @@ class RateLimitException(Exception):
 # =============================================================================
 WORKER_URL = os.environ.get("TURSO_WORKER_URL")
 
-PROCESS_LIMIT = 10
+PROCESS_LIMIT = 1000
 
 START_TIME = time.time()
 MAX_RUNTIME_SECONDS = 5 * 60 * 60 + 15 * 60
